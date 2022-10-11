6acede748142b5b6ea0a99d5e9639265e0bec2c8
Mehdi <mehdi66allam@gmail.com>
2022-10-11 23:30:10 +0100

    First try

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
+'use strict';
 console.log("Hello, World!");
+
+const menu = document.getElementById("aside");
+
+function openMenu() {
+	menu.classList.add("opened");
+	document.body.classList.add("stop-scroll");
+}
+
+function closeMenu() {
+	menu.classList.remove("opened");
+	document.body.classList.remove("stop-scroll");
+}
+
+const menuOpenBtn = document.getElementById("menu-open-btn");
+const menuCloseBtn = document.getElementById("menu-close-btn");
+
+menuOpenBtn.addEventListener("click", openMenu);
+menuCloseBtn.addEventListener("click", closeMenu);
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
